ad46ecdfa6f883fb243ec58a3822ce77a2eb14c4
sungjaeyeol <37139204+sungjaeyeol@users.noreply.github.com>
2019-11-27 09:23:51 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView backBtn, checkBtn, dateBtn;
+    ImageView backBtn, checkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
         backBtn = findViewById(R.id.backBtn);
         checkBtn = findViewById(R.id.checkBtn);
-        dateBtn = findViewById(R.id.dateBtn);
+        //dateBtn = findViewById(R.id.dateBtn);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,13 +28,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "저장하기", Toast.LENGTH_SHORT).show();
-            }
-        });
-        dateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, calendar.class);
-                startActivity(intent);
             }
         });
     }
